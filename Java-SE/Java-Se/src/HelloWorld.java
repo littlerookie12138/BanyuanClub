9c3a307b40102d11f283b46d2e952c9f9356dce3
zwy <edz@edzdeMacBook-Air.local>
2020-06-08 17:31:07 +0800

    2020.6.8作业

--- Java-SE/Java-Se/src/HelloWorld.java
+++ Java-SE/Java-Se/src/HelloWorld.java
@@ -1,5 +1,25 @@
+import java.io.Console;
+import java.util.Scanner;
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!!");
+
+//        Scanner in = new Scanner(System.in);
+//        String name = in.next();//遇到空格停止
+//        String namee = in.nextLine();//读取一整行
+//        int age = in.nextInt();//读取一个整数
+
+        /*
+        * Console类隐藏输入
+        */
+        Console cons =System.console();
+        String username = cons.readLine("Username: ");
+        char pass[] = cons.readPassword("Password: ");
+
+
+//        System.out.println(name);
+        System.out.println(username);
+        System.out.println(pass);
     }
 }
